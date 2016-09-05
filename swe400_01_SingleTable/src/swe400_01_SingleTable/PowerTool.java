06ba9369415bf7568ad4002e8aa4b5edb78b98ee
Alec Waddelow <alec.waddelow@gmail.com>
2016-09-05 12:38:38 -0400

    PowerTool class and test class built and working fine

--- swe400_01_SingleTable/src/swe400_01_SingleTable/PowerTool.java
+++ swe400_01_SingleTable/src/swe400_01_SingleTable/PowerTool.java
@@ -1,5 +1,7 @@
 package swe400_01_SingleTable;
 
+import java.sql.SQLException;
+
 /**
  * @author Drew Rife & Alec Waddelow
  *
@@ -8,8 +10,60 @@ public class PowerTool extends InventoryItem
 {
 	public boolean batteryPowered;
 
-	public PowerTool()
+	/**
+	 * Finder Constructor
+	 * @author Alec Waddelow 
+	 * 
+	 * @param id
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 */
+	public PowerTool(int id) throws ClassNotFoundException, SQLException
 	{
+		this.id = id;
+		Runner runner = new Runner();
+		DBReturnSet dbrs;
+		dbrs = runner.queryDB(this.id);
 
+		this.upc = dbrs.getUpc();
+		this.manufacturerID = dbrs.getManufacturerID();
+		this.price = dbrs.getPrice();
+		this.batteryPowered = dbrs.isBatteryPowered();
+	}
+	
+	/**
+	 * Creation Constructor
+	 * @author Alec Waddelow 
+	 * 
+	 * @param id
+	 * @param upc
+	 * @param manufacturerID
+	 * @param price
+	 * @param description
+	 * @param batteryPowered
+	 * @param length
+	 * @param numberInStrip
+	 * @param numberInBox
+	 * @param className
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 */
+	public PowerTool (int id, String upc, int manufacturerID, int price, String description,
+			boolean batteryPowered, long length, int numberInStrip, int numberInBox, String className) throws ClassNotFoundException, SQLException
+	{
+		this.id = id;
+		this.upc = upc;
+		this.manufacturerID = manufacturerID;
+		this.price = price;
+		this.batteryPowered = batteryPowered;
+		
+		DBReturnSet dbrs = new DBReturnSet();
+		dbrs.setId(this.id);
+		dbrs.setUpc(this.upc);
+		dbrs.setManufacturerID(this.manufacturerID);
+		dbrs.setPrice(this.price);
+		dbrs.setBatteryPowered(this.batteryPowered);
+		Runner runner = new Runner();
+		runner.insertRow(dbrs);
 	}
 }
--- swe400_01_SingleTable/test/swe400_01_SingleTable/TestPowerTool.java
+++ swe400_01_SingleTable/test/swe400_01_SingleTable/TestPowerTool.java
@@ -5,6 +5,8 @@ package swe400_01_SingleTable;
 
 import static org.junit.Assert.*;
 
+import java.sql.SQLException;
+
 import org.junit.Test;
 
 /**
@@ -15,9 +17,22 @@ public class TestPowerTool
 {
 
 	@Test
-	public void test() 
+	public void testInitialization() throws ClassNotFoundException, SQLException 
 	{
-		fail("Not yet implemented");
+		PowerTool pt = new PowerTool(6);
+		assertEquals("6", pt.upc);
+		assertEquals(6, pt.manufacturerID);
+		assertEquals(6, pt.price);
+		assertEquals(true,pt.batteryPowered);
 	}
-
+	
+	@Test
+	public void testInsertingObjectIntoRow() throws ClassNotFoundException, SQLException
+	{
+		PowerTool pt = new PowerTool(7, "7", 7, 7, "", false, 0, 0, 0, "PowerTool");
+		assertEquals("7", pt.upc);
+		assertEquals(7, pt.manufacturerID);
+		assertEquals(7, pt.price);
+	} 
+	
 }
