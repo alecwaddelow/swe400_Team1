05ce30e27c95ac36f683d7f0e77221aaf10e093e
drew <dr5801@cs.ship.edu>
2016-09-09 15:51:35 -0400

    added the createdatabse class

---
+++ swe400_01_SingleTable/src/swe400_01_SingleTable/CreateDatabase.java
@@ -0,0 +1,10 @@
+package swe400_01_SingleTable;
+
+/**
+ * @author Drew Rife & Alec Waddelow
+ *
+ */
+public class CreateDatabase
+{
+
+}
--- swe400_01_SingleTable/src/swe400_01_SingleTable/Runner.java
+++ swe400_01_SingleTable/src/swe400_01_SingleTable/Runner.java
@@ -78,7 +78,6 @@ public class Runner
 		return null;
 
 	}
-
 }
 
 
