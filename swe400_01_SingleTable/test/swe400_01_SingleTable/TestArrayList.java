443be1e76f5bddf82f189baea52c5b05e4da176f
drew <dr5801@cs.ship.edu>
2016-09-10 15:39:19 -0400

    added retrieval test for tool from array list but getting an error that we cannot cast it like what we did with the nail retrieval test

--- swe400_01_SingleTable/test/swe400_01_SingleTable/TestArrayList.java
+++ swe400_01_SingleTable/test/swe400_01_SingleTable/TestArrayList.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
 import org.junit.Test;
 
 import enums.Nails;
+import enums.Tools;
 
 /**
  * @author Alec Waddelow & Drew Rife
@@ -19,29 +20,52 @@ import enums.Nails;
 public class TestArrayList
 {
 	private static int uniqueTestID = 1;
+	private static int indexOfArrayList = 0;
+
 	/**
-	 * Tests creation of the ArrayList from the database
+	 * Tests getting the nail objects from the array list
 	 *
 	 * @throws ClassNotFoundException
 	 * @throws SQLException
 	 */
 	@Test
-	public void testRetrieveEnumNail() throws ClassNotFoundException, SQLException
+	public void testRetrieveNailsFromArrayList() throws ClassNotFoundException, SQLException
 	{
 		ArrayList<Object> returnSet = new ArrayList<Object>();
 		returnSet = Runner.createList();
 
 		for(int i = 0; i < Nails.values().length; i++)
 		{
-			Nail n = Nail.class.cast(returnSet.get(i));
+			Nail n = Nail.class.cast(returnSet.get(indexOfArrayList));
 			assertEquals(uniqueTestID, n.getId());
-			assertEquals(Nails.values()[i].getUpc(), n.getUpc());
-			assertEquals(Nails.values()[i].getManufacturerID(), n.getManufacturerID());
-			assertEquals(Nails.values()[i].getLength(), n.getLength(), 0.001);
-			assertEquals(Nails.values()[i].getPrice(), n.getPrice());
-			assertEquals(Nails.values()[i].getNumberInBox(), n.getNumberInBox());
+			assertEquals(Nails.values()[indexOfArrayList].getUpc(), n.getUpc());
+			assertEquals(Nails.values()[indexOfArrayList].getManufacturerID(), n.getManufacturerID());
+			assertEquals(Nails.values()[indexOfArrayList].getLength(), n.getLength(), 0.001);
+			assertEquals(Nails.values()[indexOfArrayList].getPrice(), n.getPrice());
+			assertEquals(Nails.values()[indexOfArrayList].getNumberInBox(), n.getNumberInBox());
+			indexOfArrayList++;
 			uniqueTestID++;
 		}
 	}
+
+	@Test
+	public void testRetrieveToolsFromArrayList() throws ClassNotFoundException, SQLException
+	{
+		ArrayList<Object> returnSet = new ArrayList<Object>();
+		returnSet = Runner.createList();
+
+		for(int i = 0; i < Tools.values().length; i++)
+		{
+			Tool t = Tool.class.cast(returnSet.get(indexOfArrayList));
+			assertEquals(uniqueTestID, t.getId());
+
+		}
+	}
+
+
+
+
+
+
 
 }
