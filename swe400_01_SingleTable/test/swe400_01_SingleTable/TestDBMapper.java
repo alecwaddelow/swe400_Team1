c578089109d2b1da586340994b9083a8b2a42340
dr5801 <dr5801@cs.ship.edu>
2016-11-01 18:08:14 -0400

    added tests for setters in DBMapper

--- swe400_01_SingleTable/test/swe400_01_SingleTable/TestDBMapper.java
+++ swe400_01_SingleTable/test/swe400_01_SingleTable/TestDBMapper.java
@@ -27,7 +27,6 @@ public class TestDBMapper
 		public MockDBMapper(String upc, int manufacturerID, int price, String className) 
 		{
 			super(upc,manufacturerID, price, className);
-			
 		}
 		
 	}
@@ -40,5 +39,29 @@ public class TestDBMapper
 	{
 		MockDBMapper mapper = new MockDBMapper(null, 0, 0, null);
 		assertTrue(mapper instanceof DBMapper);
+	}
+	
+	/**
+	 * Tests setters in DBMapper
+	 */
+	@Test
+	public void testSetters()
+	{
+		MockDBMapper mapper = new MockDBMapper(null, 0, 0, null);
+		
+		assertEquals(null, mapper.getUpc());
+		assertEquals(0, mapper.getManufacturerID());
+		assertEquals(0, mapper.getPrice());
+		assertEquals(null, mapper.getClassName());
+		
+		mapper.setClassName("Tool");
+		mapper.setManufacturerID(30);
+		mapper.setPrice(2);
+		mapper.setUpc("30303030");
+		
+		assertEquals("30303030", mapper.getUpc());
+		assertEquals(30, mapper.getManufacturerID());
+		assertEquals(2, mapper.getPrice());
+		assertEquals("Tool", mapper.getClassName());
 	}
 }
