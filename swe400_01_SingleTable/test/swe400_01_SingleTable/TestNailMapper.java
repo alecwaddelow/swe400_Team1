83abd5beabd98c5c31dd98c0a933a918734d0f96
dr5801 <dr5801@cs.ship.edu>
2016-11-01 18:29:55 -0400

    Tested setters in NailMapper

--- swe400_01_SingleTable/test/swe400_01_SingleTable/TestNailMapper.java
+++ swe400_01_SingleTable/test/swe400_01_SingleTable/TestNailMapper.java
@@ -9,6 +9,7 @@ import java.sql.SQLException;
 
 import org.junit.Test;
 
+import domain_layer.Nail;
 import domain_layer.NailMapper;
 
 /**
@@ -35,5 +36,28 @@ public class TestNailMapper
 		assertEquals(5.0, nm.getLength(), 0.001);
 		assertEquals(0, nm.getNumberInBox());
 		assertEquals("NailMapper", nm.getClassName());
+	}
+	
+	/**
+	 * tests setters for nail mapper
+	 */
+	@Test
+	public void testSetters()
+	{
+		NailMapper nailMapper = new NailMapper();
+		
+		nailMapper.setUpc("3");
+		nailMapper.setManufacturerID(4);
+		nailMapper.setPrice(40);
+		nailMapper.setLength(4.5);
+		nailMapper.setNumberInBox(4);
+		nailMapper.setClassName("Nail");
+		
+		assertEquals("3", nailMapper.getUpc());
+		assertEquals(4, nailMapper.getManufacturerID());
+		assertEquals(40, nailMapper.getPrice());
+		assertEquals(4.5, nailMapper.getLength(), 0.001);
+		assertEquals(4, nailMapper.getNumberInBox());
+		assertEquals("Nail", nailMapper.getClassName());
 	}
 }
