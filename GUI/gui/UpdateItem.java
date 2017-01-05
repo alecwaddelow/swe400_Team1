ae71c02033b9423a1f8460ad74854fb07e634705
dr5801 <dr5801@cs.ship.edu>
2017-01-05 18:53:36 -0500

    created a new gui class

---
+++ GUI/gui/UpdateItem.java
@@ -0,0 +1,47 @@
+package gui;
+
+import java.awt.EventQueue;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JFrame;
+import domain.*;
+
+public class UpdateItem 
+{
+
+	private JFrame frame;
+	private static List<InventoryItem> inventoryItems = new ArrayList<InventoryItem>();
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					UpdateItem window = new UpdateItem();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public UpdateItem() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
